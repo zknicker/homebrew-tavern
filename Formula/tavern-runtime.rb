cabd6712c0eaf7250f827dcaa248a9418bba3109
Zach Knickerbocker <zknicker@gmail.com>
2026-05-30 00:28:33 -0400

    tavern-runtime 1.1.1

---
+++ Formula/tavern-runtime.rb
@@ -0,0 +1,33 @@
+class TavernRuntime < Formula
+  desc "Always-on Tavern Runtime server"
+  homepage "https://github.com/zknicker/tavern"
+  url "https://punchpress-electron-app-209596837609-us-east-1-an.s3.us-east-1.amazonaws.com/tavern/mac/tavern-runtime-1.1.1-aarch64-apple-darwin.tar.gz"
+  sha256 "eb13f07e2b04d1f9d98fb69ca399f88656995daac807528250fdc6c5910e869b"
+  version "1.1.1"
+  license :cannot_represent
+
+  depends_on "node"
+
+  def install
+    bin.install "bin/tavern-runtime"
+    (share/"tavern").install "share/tavern/openclaw-plugins"
+    (share/"tavern/node_modules/@tavern").install "share/tavern/node_modules/@tavern/sdk"
+    (etc/"tavern").mkpath
+    (var/"tavern/runtime").mkpath
+    (var/"log/tavern").mkpath
+  end
+
+  service do
+    run [opt_bin/"tavern-runtime", "serve"]
+    environment_variables TAVERN_RUNTIME_HOST: "127.0.0.1",
+      TAVERN_RUNTIME_PORT: "4310",
+      TAVERN_RUNTIME_ROOT: var/"tavern/runtime"
+    keep_alive true
+    log_path var/"log/tavern/runtime.log"
+    error_log_path var/"log/tavern/runtime.log"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/tavern-runtime --version")
+  end
+end
