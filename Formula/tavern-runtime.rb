a5f855bd6663eeabc222b6c53d5fd606e1c9de7e
Zach Knickerbocker <zknicker@gmail.com>
2026-07-02 10:43:11 -0400

    tavern-runtime 1.4.34

--- Formula/tavern-runtime.rb
+++ Formula/tavern-runtime.rb
@@ -1,9 +1,9 @@
 class TavernRuntime < Formula
   desc "Always-on Tavern Runtime server"
   homepage "https://github.com/zknicker/tavern"
-  url "https://punchpress-electron-app-209596837609-us-east-1-an.s3.us-east-1.amazonaws.com/tavern/mac/tavern-runtime-1.4.33-aarch64-apple-darwin.tar.gz"
-  sha256 "272b34a192ac0fb5399301ba8e0cec7cb94d6e0c75e7137c36d543adb5149fdf"
-  version "1.4.33"
+  url "https://punchpress-electron-app-209596837609-us-east-1-an.s3.us-east-1.amazonaws.com/tavern/mac/tavern-runtime-1.4.34-aarch64-apple-darwin.tar.gz"
+  sha256 "04809d457f426c4d8fc2ba7d76d02cd831d8c4fa1d9c1ab130884ba1cd5a6ec4"
+  version "1.4.34"
   license :cannot_represent
 
   depends_on "node"
